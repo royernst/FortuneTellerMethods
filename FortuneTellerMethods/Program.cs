29aad2a2e21599fce35f5fdc8d3717ff25c4a863
royernst <roy.e31@hotmail.com>
2017-10-05 13:28:13 -0400

    Working through text prompts

--- FortuneTellerMethods/Program.cs
+++ FortuneTellerMethods/Program.cs
@@ -10,6 +10,35 @@ namespace FortuneTellerMethods
     {
         static void Main(string[] args)
         {
+            string firstName;
+            string lastName;
+            string quest;
+            string userAge;
+            string birthMonth;
+            string favColor;
+            string numSiblings;
+            string contResponse;
+
+            Console.WriteLine("STOP!");
+            Console.WriteLine("Who would cross the Bridge of Death");
+            Console.WriteLine("Must answer me");
+            Console.WriteLine("These questions three");
+            Console.WriteLine("Ere the other side he see.");
+            Console.WriteLine("What...is your first name?");
+            firstName = Console.ReadLine().Trim();
+            Console.WriteLine("...and your last name?");
+            lastName = Console.ReadLine().Trim();
+            Console.WriteLine("What...is your quest?");
+            quest = Console.ReadLine().Trim().ToLower();
+            Console.WriteLine("What...is your favorite color? (ROYGBIV only please -- if you aren't sure what that is, type \"Help\".");
+            favColor = Console.ReadLine().Trim().ToLower();
+            Console.WriteLine("Right, off you go.");
+            Console.WriteLine("Wait! Wait a moment!  Before leave, I could read your fortune if you'd like.  I just need a bit more information.");
+            Console.WriteLine("Would you be interested? (YES/NO)");
+            contResponse = Console.ReadLine().Trim().ToLower();
+
+
+
         }
     }
 }
